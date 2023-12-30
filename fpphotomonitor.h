680fe7488ea3f06db4f0c34626585e0bf633f6aa
Luca Carlon <carlon.luca@gmail.com>
2023-12-30 20:47:10 +0100

    Make the model re-usable.

--- fpphotomonitor.h
+++ fpphotomonitor.h
@@ -30,49 +30,14 @@
 #include <QAbstractListModel>
 
 #include <lqtutils_prop.h>
+#include <lqtutils_models.h>
 
 #include "rest/fpflashbackyearsrequest.h"
-
-template<typename T>
-class QmlSharedPointerList : public QAbstractListModel
-{
-public:
-    QmlSharedPointerList<T>(const QList<QSharedPointer<T>>& list = QList<QSharedPointer<T>>(), QObject* parent = nullptr) :
-        QAbstractListModel(parent), m_list(list) {}
-
-    int rowCount(const QModelIndex& parent = QModelIndex()) const override {
-        return static_cast<int>(m_list.count());
-    }
-
-    QVariant data(const QModelIndex& index, int role = Qt::UserRole) const override {
-        if (role != Qt::UserRole)
-            return QVariant();
-        if (index.row() < 0 || index.row() >= m_list.size())
-            return QVariant();
-
-        return QVariant::fromValue<T*>(m_list[index.row()].data());
-    }
-
-    QHash<int, QByteArray> roleNames() const override {
-        QHash<int, QByteArray> roles;
-        roles.insert(Qt::UserRole, "data");
-        return roles;
-    }
-
-    void refreshModel(const QList<QSharedPointer<T>>& list) {
-        beginResetModel();
-        m_list = list;
-        endResetModel();
-    }
-
-private:
-    QList<QSharedPointer<T>> m_list;
-};
 
 class FPPhotoMonitor : public QObject
 {
     Q_OBJECT
-    L_RW_PROP_AS(QmlSharedPointerList<FPFlashbackYear>*, flashbackYears, new QmlSharedPointerList(QList<QSharedPointer<FPFlashbackYear>>(), this))
+    L_RW_PROP_AS(lqt::QmlSharedPointerList<FPFlashbackYear>*, flashbackYears, new lqt::QmlSharedPointerList(QList<QSharedPointer<FPFlashbackYear>>(), this))
     L_RW_PROP_AS(bool, working, false)
 public:
     explicit FPPhotoMonitor(QObject* parent = nullptr);
